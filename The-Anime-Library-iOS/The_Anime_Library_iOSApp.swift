8c2dea8d99673743a00ff44faa15806cee6637d2
Derrick Garcon <dgarconpro@gmail.com>
2023-09-06 15:08:25 -0400

    Initial Commit

---
+++ The-Anime-Library-iOS/The_Anime_Library_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Anime_Library_iOSApp.swift
+//  The-Anime-Library-iOS
+//
+//  Created by Derrick on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Anime_Library_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
